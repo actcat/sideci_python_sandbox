170a2421a37cf69fb1cd1ad1cb17ca0ddc87b3db
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-15 14:08:43 +0900

    Add dirty codes

--- lib/add.py
+++ lib/add.py
@@ -1,2 +1,17 @@
 def add(x,y):
     return x + y
+
+# https://ep2013.europython.eu/media/conference/slides/lets-code-with-style.html
+x             = 1
+y             = 2
+long_variable = 3
+
+foo = long_function_name(one, two,
+    three, four)
+
+my_list = [
+        (1,2,3),
+        (4,5,6),
+        ]
+
+print my_list
